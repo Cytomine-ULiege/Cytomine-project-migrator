1e900cd7cc9624aefaace6174e64fc247a1d0379
Ba Thien Le <bathien.le.pro@gmail.com>
2023-03-10 16:23:50 +0100

    refactor: move each export to a method

--- cytomineprojectmigrator/exporter.py
+++ cytomineprojectmigrator/exporter.py
@@ -99,178 +99,6 @@ class Exporter:
 
         self.users = UserCollection()
 
-    def run(self):
-        """Export a Cytomine project"""
-
-        logging.info("Export will be done in directory %s", self.project_path)
-        os.makedirs(self.project_path)
-
-        if self.with_metadata or self.with_annotation_metadata:
-            self.attached_file_path = os.path.join(self.project_path, "attached_files")
-            os.makedirs(self.attached_file_path)
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("1/ Export project %s", self.project.id)
-        self.save_object(self.project)
-
-        logging.info("1.1/ Export project managers")
-        admins = UserCollection(admin=True).fetch_with_filter(
-            "project", self.project.id
-        )
-        for admin in admins:
-            self.save_user(admin, "project_manager")
-
-        logging.info("1.2/ Export project contributors")
-        users = UserCollection().fetch_with_filter("project", self.project.id)
-        for user in users:
-            self.save_user(user, "project_contributor")
-
-        if self.with_metadata:
-            logging.info("1.3/ Export project metadata")
-            self.export_metadata([self.project])
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("2/ Export ontology %s", self.project.ontology)
-        ontology = Ontology().fetch(self.project.ontology)
-        self.save_object(ontology)
-
-        logging.info("2.1/ Export ontology creator")
-        user = User().fetch(ontology.user)
-        self.save_user(user, "ontology_creator")
-
-        if self.with_metadata:
-            logging.info("2.2/ Export ontology metadata")
-            self.export_metadata([ontology])
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("3/ Export terms")
-        terms = TermCollection().fetch_with_filter("project", self.project.id)
-        self.save_object(terms)
-
-        if self.with_metadata:
-            logging.info("3.1/ Export term metadata")
-            self.export_metadata(terms)
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("4/ Export images")
-        images = ImageInstanceCollection().fetch_with_filter("project", self.project.id)
-        self.save_object(images)
-
-        if self.with_image_download:
-            image_path = os.path.join(self.project_path, "images")
-            os.makedirs(image_path)
-
-            def _download_image(image, path):
-                logging.info("Download file for image %s", image)
-                image.download(
-                    os.path.join(path, image.originalFilename),
-                    override=False,
-                    parent=True,
-                )
-
-            # Temporary use threading as backend, as we need to connect to Cytomine in every other processes.
-            Parallel(n_jobs=-1, backend="threading")(
-                delayed(_download_image)(image, image_path) for image in images
-            )
-
-        logging.info("4.1/ Export image creator users")
-        image_users = set([image.user for image in images])
-        for image_user in image_users:
-            user = User().fetch(image_user)
-            self.save_user(user, "image_creator")
-
-        logging.info("4.2/ Export image reviewer users")
-        image_users = set([image.reviewUser for image in images if image.reviewUser])
-        for image_user in image_users:
-            user = User().fetch(image_user)
-            self.save_user(user, "image_reviewer")
-
-        if self.with_metadata:
-            logging.info("4.3/ Export image metadata")
-            self.export_metadata(images)
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("4/ Export user annotations")
-        user_annotations = AnnotationCollection(
-            showWKT=True, showTerm=True, project=self.project.id
-        ).fetch()
-        self.save_object(user_annotations, filename="user-annotation-collection")
-
-        logging.info("4.1/ Export user annotation creator users")
-        annotation_users = set([annotation.user for annotation in user_annotations])
-        for annotation_user in annotation_users:
-            user = User().fetch(annotation_user)
-            self.save_user(user, "userannotation_creator")
-
-        logging.info("4.2/ Export user annotation term creator users")
-        annotation_users = set()
-        for annotation in user_annotations:
-            annotation_users = annotation_users.union(
-                *[
-                    set(item["user"])
-                    for item in annotation.userByTerm
-                    if annotation.userByTerm
-                ]
-            )
-
-        for annotation_user in annotation_users:
-            user = User().fetch(annotation_user)
-            self.save_user(user, "userannotationterm_creator")
-
-        if self.with_annotation_metadata:
-            logging.info("4.3/ Export user annotation metadata")
-            self.export_metadata(user_annotations)
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("5/ Export users")
-        if self.anonymize:
-            for i, user in enumerate(self.users):
-                user.username = f"anonymized_user{i + 1}"
-                user.firstname = "Anonymized"
-                user.lastname = f"User {i + 1}"
-                user.email = f"anonymous{i + 1}@unknown.com"
-
-        self.save_object(self.users)
-
-        # Disabled due to core issue.
-        # if self.with_metadata:
-        #     logging.info("5.1/ Export user metadata")
-        #     self.export_metadata(self.users)
-
-        # --------------------------------------------------------------------------------------------------------------
-        if self.with_image_groups:
-            logging.info("6/ Export image groups")
-            image_groups = ImageGroupCollection().fetch_with_filter(
-                "project", self.project.id
-            )
-            self.save_object(image_groups)
-
-            if self.with_metadata:
-                logging.info("6.1/ Export image group metadata")
-                self.export_metadata(image_groups)
-
-            if self.with_image_download:
-                image_group_path = os.path.join(self.project_path, "imagegroups")
-                os.makedirs(image_group_path)
-                for image_group in image_groups:
-                    image_group.download(
-                        os.path.join(image_group_path, image_group.name),
-                        override=False,
-                        parent=True,
-                    )
-
-            image_sequences = ImageGroupImageInstanceCollection()
-            for image_group in image_groups:
-                image_sequences += (
-                    ImageGroupImageInstanceCollection().fetch_with_filter(
-                        "imagegroup", image_group.id
-                    )
-                )
-            self.save_object(image_sequences)
-
-        # --------------------------------------------------------------------------------------------------------------
-        logging.info("Finished.")
-
     def export_metadata(self, objects):
         """Export the medata"""
 
@@ -320,9 +148,207 @@ class Exporter:
         elif isinstance(obj, Collection):
             filename = f"{obj.callback_identifier}-collection.json"
 
-        with open(os.path.join(self.project_path, filename), "w") as outfile:
+        with open(
+            os.path.join(self.project_path, filename), "w", encoding="utf-8"
+        ) as outfile:
             outfile.write(obj.to_json())
             logging.info("Object %s has been saved locally.", obj)
+
+    def export_project(self):
+        """Export a Cytomine project"""
+
+        self.save_object(self.project)
+
+        logging.info("1.1/ Export project managers")
+        admins = UserCollection(admin=True).fetch_with_filter(
+            "project", self.project.id
+        )
+        for admin in admins:
+            self.save_user(admin, "project_manager")
+
+        logging.info("1.2/ Export project contributors")
+        users = UserCollection().fetch_with_filter("project", self.project.id)
+        for user in users:
+            self.save_user(user, "project_contributor")
+
+        if self.with_metadata:
+            logging.info("1.3/ Export project metadata")
+            self.export_metadata([self.project])
+
+    def export_ontology(self):
+        """Export an ontology"""
+
+        ontology = Ontology().fetch(self.project.ontology)
+        self.save_object(ontology)
+
+        logging.info("2.1/ Export ontology creator")
+        user = User().fetch(ontology.user)
+        self.save_user(user, "ontology_creator")
+
+        if self.with_metadata:
+            logging.info("2.2/ Export ontology metadata")
+            self.export_metadata([ontology])
+
+    def export_terms(self):
+        """Export terms"""
+
+        terms = TermCollection().fetch_with_filter("project", self.project.id)
+        self.save_object(terms)
+
+        if self.with_metadata:
+            logging.info("3.1/ Export term metadata")
+            self.export_metadata(terms)
+
+    def export_images(self):
+        """Export images"""
+
+        images = ImageInstanceCollection().fetch_with_filter("project", self.project.id)
+        self.save_object(images)
+
+        if self.with_image_download:
+            image_path = os.path.join(self.project_path, "images")
+            os.makedirs(image_path)
+
+            def _download_image(image, path):
+                logging.info("Download file for image %s", image)
+                image.download(
+                    os.path.join(path, image.originalFilename),
+                    override=False,
+                    parent=True,
+                )
+
+            # Temporary use threading as backend, as we need to connect to Cytomine in every other processes.
+            Parallel(n_jobs=-1, backend="threading")(
+                delayed(_download_image)(image, image_path) for image in images
+            )
+
+        logging.info("4.1/ Export image creator users")
+        image_users = set(image.user for image in images)
+        for image_user in image_users:
+            user = User().fetch(image_user)
+            self.save_user(user, "image_creator")
+
+        logging.info("4.2/ Export image reviewer users")
+        image_users = set(image.reviewUser for image in images if image.reviewUser)
+        for image_user in image_users:
+            user = User().fetch(image_user)
+            self.save_user(user, "image_reviewer")
+
+        if self.with_metadata:
+            logging.info("4.3/ Export image metadata")
+            self.export_metadata(images)
+
+    def export_annotations(self):
+        """Export user annotations"""
+
+        user_annotations = AnnotationCollection(
+            showWKT=True, showTerm=True, project=self.project.id
+        ).fetch()
+        self.save_object(user_annotations, filename="user-annotation-collection")
+
+        logging.info("4.1/ Export user annotation creator users")
+        annotation_users = set(annotation.user for annotation in user_annotations)
+        for annotation_user in annotation_users:
+            user = User().fetch(annotation_user)
+            self.save_user(user, "userannotation_creator")
+
+        logging.info("4.2/ Export user annotation term creator users")
+        annotation_users = set()
+        for annotation in user_annotations:
+            annotation_users = annotation_users.union(
+                *[
+                    set(item["user"])
+                    for item in annotation.userByTerm
+                    if annotation.userByTerm
+                ]
+            )
+
+        for annotation_user in annotation_users:
+            user = User().fetch(annotation_user)
+            self.save_user(user, "userannotationterm_creator")
+
+        if self.with_annotation_metadata:
+            logging.info("4.3/ Export user annotation metadata")
+            self.export_metadata(user_annotations)
+
+    def export_users(self):
+        """Export users of the project"""
+
+        if self.anonymize:
+            for i, user in enumerate(self.users):
+                user.username = f"anonymized_user{i + 1}"
+                user.firstname = "Anonymized"
+                user.lastname = f"User {i + 1}"
+                user.email = f"anonymous{i + 1}@unknown.com"
+
+        self.save_object(self.users)
+
+        # Disabled due to core issue.
+        # if self.with_metadata:
+        #     logging.info("5.1/ Export user metadata")
+        #     self.export_metadata(self.users)
+
+    def export_image_groups(self):
+        """Export image groups"""
+        image_groups = ImageGroupCollection().fetch_with_filter(
+            "project", self.project.id
+        )
+        self.save_object(image_groups)
+
+        if self.with_metadata:
+            logging.info("6.1/ Export image group metadata")
+            self.export_metadata(image_groups)
+
+        if self.with_image_download:
+            image_group_path = os.path.join(self.project_path, "imagegroups")
+            os.makedirs(image_group_path)
+            for image_group in image_groups:
+                image_group.download(
+                    os.path.join(image_group_path, image_group.name),
+                    override=False,
+                    parent=True,
+                )
+
+        image_sequences = ImageGroupImageInstanceCollection()
+        for image_group in image_groups:
+            image_sequences += ImageGroupImageInstanceCollection().fetch_with_filter(
+                "imagegroup", image_group.id
+            )
+        self.save_object(image_sequences)
+
+    def run(self):
+        """Serialize a Cytomine project"""
+
+        logging.info("Export will be done in directory %s", self.project_path)
+        os.makedirs(self.project_path)
+
+        if self.with_metadata or self.with_annotation_metadata:
+            self.attached_file_path = os.path.join(self.project_path, "attached_files")
+            os.makedirs(self.attached_file_path)
+
+        logging.info("1/ Export project %s", self.project.id)
+        self.export_project()
+
+        logging.info("2/ Export ontology %s", self.project.ontology)
+        self.export_ontology()
+
+        logging.info("3/ Export terms")
+        self.export_terms()
+
+        logging.info("4/ Export images")
+        self.export_images()
+
+        logging.info("4/ Export user annotations")
+        self.export_annotations()
+
+        logging.info("5/ Export users")
+        self.export_users()
+
+        if self.with_image_groups:
+            logging.info("6/ Export image groups")
+            self.export_image_groups()
+
+        logging.info("Finished.")
 
     def make_archive(self):
         """Create an archive of the project"""
